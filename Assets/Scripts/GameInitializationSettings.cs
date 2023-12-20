872ce691658252596c4e84d9bdc9ee8820d4a280
Jordy Perret <jordy.perret@edu-kw1c.nl>
2023-12-20 12:40:35 +0100

    V1.1 - Final Boss Spawn System Update

--- Assets/Scripts/GameInitializationSettings.cs
+++ Assets/Scripts/GameInitializationSettings.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameInitializationSettings : MonoBehaviour
 {
     public bool roundInProgress = false;
+    public bool finalBoss = false;
     public bool playerDied = false;
 
     // Start is called before the first frame update
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -15,11 +15,19 @@ public class Spawner : MonoBehaviour
     public GameObject generalScripts;
     private GameInitializationSettings gameInitializationSettings;
 
+    private finalBossSetting;
+
     void Start()
     {
         // Start spawning objects at the specified rate
         gameInitializationSettings = generalScripts.GetComponent<GameInitializationSettings>();
+
         InvokeRepeating("SpawnObject", 0f, spawnRate);
+    }
+
+    void Update()
+    {
+
     }
 
     void SpawnObject()
@@ -28,47 +36,53 @@ public class Spawner : MonoBehaviour
         {
             if (gameInitializationSettings.roundInProgress == true)
             {
-                // Spawn ruimte defineren
-                Vector2 randomSpawnPoint = new Vector2
-                (
-                    Random.Range(transform.position.x - spawnArea.x / 2, transform.position.x + spawnArea.x / 2),
-                    Random.Range(transform.position.y - spawnArea.y / 2, transform.position.y + spawnArea.y / 2)
-                );
-
-                // scoreCount defineren
-                ScoreCount scoreCount = scoreCounterGameObject.GetComponent<ScoreCount>();
-
-                // Check of scoreCount object opgehaald wordt
-                if (scoreCount != null)
+                if (gameInitializationSettings.finalBoss == false)
                 {
+                    // Spawn ruimte defineren
+                    Vector2 randomSpawnPoint = new Vector2
+                    (
+                        Random.Range(transform.position.x - spawnArea.x / 2, transform.position.x + spawnArea.x / 2),
+                        Random.Range(transform.position.y - spawnArea.y / 2, transform.position.y + spawnArea.y / 2)
+                    );
 
-                    Debug.Log("Spawner: Score = " + scoreCount.score.ToString());
+                    // Spawn normale enemies
+                    Instantiate(prefabToSpawn, randomSpawnPoint, Quaternion.identity);
 
-                    // Check of score lager is dan 
-                    if (scoreCount.score >= 60)
+                    // EnemyController defineren vanuit gespawnde enemy
+                    EnemyController enemyController = prefabToSpawn.GetComponent<EnemyController>();
+
+                    // Checken of EnemyController bestaat
+                    if (enemyController != null)
                     {
-                        // Spawn final boss
-                        Instantiate(gigaMarcelToSpawn, randomSpawnPoint, Quaternion.identity);
-                        Spawner spawner = parentObject.GetComponent<Spawner>();
-                        Destroy(spawner);
+                        // Scripts toevoegen aan enemy zodat de scores en ammo bijgehouden wordt.
+                        enemyController.AssignGameObjects(scoreCounterGameObject, AmmoCounterGameObject);
                     }
-                    else
-                    {
-                        // Spawn normale enemies
-                        Instantiate(prefabToSpawn, randomSpawnPoint, Quaternion.identity);
-
-                        // EnemyController defineren vanuit gespawnde enemy
-                        EnemyController enemyController = prefabToSpawn.GetComponent<EnemyController>();
-
-                        // Checken of EnemyController bestaat
-                        if (enemyController != null)
-                        {
-                            // Scripts toevoegen aan enemy zodat de scores en ammo bijgehouden wordt.
-                            enemyController.AssignGameObjects(scoreCounterGameObject, AmmoCounterGameObject);
-                        }
-                    }
-                }
+                }                
             }
         }        
+    }
+
+    public void SpawnBoss()
+    {
+        if (gameInitializationSettings != null)
+        {
+            if (gameInitializationSettings.roundInProgress == true)
+            {
+                if (gameInitializationSettings.finalBoss == true)
+                {
+                    // Spawn ruimte defineren
+                    Vector2 randomSpawnPoint = new Vector2
+                    (
+                        Random.Range(transform.position.x - spawnArea.x / 2, transform.position.x + spawnArea.x / 2),
+                        Random.Range(transform.position.y - spawnArea.y / 2, transform.position.y + spawnArea.y / 2)
+                    );
+
+                    // Spawn final boss
+                    Instantiate(gigaMarcelToSpawn, randomSpawnPoint, Quaternion.identity);
+                    Spawner spawner = parentObject.GetComponent<Spawner>();
+                    Destroy(spawner);
+                }
+            }
+        }
     }
 }
