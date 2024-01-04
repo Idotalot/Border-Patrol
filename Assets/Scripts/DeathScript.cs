cbd7d41aadb404217c81adb6303dae77de6f444f
Jordy Perret <jordy.perret@edu-kw1c.nl>
2024-01-04 22:02:03 +0100

    V1.4 - Fixed Deathscreen

--- Assets/Scripts/DeathScript.cs
+++ Assets/Scripts/DeathScript.cs
@@ -1,43 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeathScript : MonoBehaviour
 {
     public List<GameObject> deathScreenObjects;
     public GameObject deathScreen;
-    // Start is called before the first frame update
-
+    public TextMeshProUGUI deathScreenText;
+    public Button deathScreenRetryButton;
+    public Button deathScreenExitButton;
+    public RectTransform retryTransform;
+    public RectTransform exitTransform;
     // Game settings defineren
     public GameObject generalScripts;
     private GameInitializationSettings gameInitializationSettings;
 
+    // Start is called before the first frame update
     void Start()
+    {
+        // Iterate through all child GameObjects using foreach
+        InvokeRepeating("popUpDeathScreen", 0f, 1f);
+    }
+
+    private void popUpDeathScreen()
     {
         gameInitializationSettings = generalScripts.GetComponent<GameInitializationSettings>();
 
         for (int i = 0; i < deathScreen.transform.childCount; i++)
         {
+            /*Vector2 retryTransformPos = new Vector2(85, )*/
             SpriteRenderer childRenderer = deathScreen.transform.GetChild(i).GetComponent<SpriteRenderer>();
-            Debug.Log(childRenderer.name + ": " + childRenderer.sortingLayerName);
-        }
-        // Iterate through all child GameObjects using foreach
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        gameInitializationSettings = generalScripts.GetComponent<GameInitializationSettings>();
-
-        if (gameInitializationSettings.playerDied == true)
-        {
-            for (int i = 0; i < deathScreen.transform.childCount; i++)
+            if (gameInitializationSettings.playerDied == true)
             {
-                SpriteRenderer childRenderer = deathScreen.transform.GetChild(i).GetComponent<SpriteRenderer>();
+                deathScreenText.text = "You suck!";
                 childRenderer.sortingLayerName = "DeathScreen";
+
+                retryTransform.anchoredPosition = new Vector2(85, -200);
+                exitTransform.anchoredPosition = new Vector2(-85, -200);
+            }
+            else if (gameInitializationSettings.playerDied == false)
+            {
+                deathScreenText.text = "";
+                childRenderer.sortingLayerName = "InvisibleLayer";
+
+                retryTransform.anchoredPosition = new Vector2(85, -300);
+                exitTransform.anchoredPosition = new Vector2(-85, -300);
             }
         }
-        
     }
 }
---
+++ Assets/Scripts/GameExitScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameExitScript : MonoBehaviour
+{
+    public Button exitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Exit requested");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
